9192ac17b67028f6ddac33679ae8f65ea68d8011
Massimo Galleri <massimo@galleri.it>
2019-02-05 01:18:30 +0100

    Update parallax-elements.js

--- parallax-elements.js
+++ parallax-elements.js
@@ -1,11 +1,11 @@
 {
 
-  function scrollingEvents() {
+  function _parallax_elements() {
 
     let _element = document.querySelectorAll('[data-parallax]');
 
     _element.forEach(function (item) {
-      let _elementH = item.clientHeight;
+      let _elementH = item.getBoundingClientRect().height;
       let _elementT = item.getBoundingClientRect().top;
       let _elementP = item.dataset.parallax;
       let _elementY = ((window.outerHeight - _elementH) / 2, _elementT - window.pageYOffset);
@@ -16,6 +16,6 @@
 
   }
 
-  window.addEventListener('scroll', scrollingEvents);
+  window.addEventListener('scroll', _parallax_elements);
 
 }
